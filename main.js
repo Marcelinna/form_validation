79c2270ec6839ea052a67030f61bd2f4c10117b7
Marcelina <marcelinasiwecka@interia.pl>
2020-04-01 00:05:53 +0200

    fc

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ function ValidationInputs() {
 		setError(username, "Username cannot be blank");
 	} else {
 		setSuccess(username);
+		
     }
     
     const check_email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
